b63f3521df7b755ce6367e0ce92e7455ece7a62d
DAM111 <DAM111@IF03-10.iesmhp.local>
2020-11-09 16:39:52 +0100

    He respondido las preguntas y las he añadido al proyecto con un push.

---
+++ Pruebapushpullcommit+/src/pruebapushpullcommit/Pruebapushpullcommit.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebapushpullcommit;
+
+/**
+ *
+ * @author DAM111
+ */
+public class Pruebapushpullcommit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("1. Además de Git, ¿que otros sistemas de control de versiones existen?");
+        System.out.println("Mercurial por ejemplo");
+
+        System.out.println("2. En Git, ¿para qué sirve el comando git config?");
+        System.out.println("configurar los valores del proyecto");
+
+        System.out.println("3. En Git, ¿para qué sirve el comando git init?");
+        System.out.println("crea un repositorio");
+
+        System.out.println("4. En Git, ¿para qué sirve el comando git clone?");
+        System.out.println("clonar un repositorio");
+
+        System.out.println("5. En Git, ¿para qué sirve el comando git status?");
+        System.out.println("para ver el estado del proyecto");
+
+        System.out.println("6. En Git, ¿para qué sirve el comando git add?");
+        System.out.println("para añadir al area de pruebas");
+        
+        
+        }
+    
+}
